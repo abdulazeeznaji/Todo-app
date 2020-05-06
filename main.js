2fbd3c1fddd48ced45315ed7ddca65fe91a30227
abdulazeeznaji <abdulazeeznaji@gmail.com>
2020-05-06 15:06:14 +0300

    Add feature to be able to add todos

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('Iam here ')
+
+
+
+// Add Todo function 
+
+function addTodo() {
+	// Add single Todo 
+	var list = document.getElementById('list')
+	var node = document.createElement("LI");                    
+	node.appendChild( document.createTextNode("task1") );                              
+	document.getElementById("list").appendChild(node);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+// 
